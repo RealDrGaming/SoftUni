21fb275b82ce8a6ebf42bb2ec04397dab182709d
Jordan <dankoneshev@abv.bg>
2023-02-18 12:05:07 +0200

    Committed

--- CSharp-Advanced/{Final Exam} 11. Final Exam/AdvancedCSharpExam/02.Second/Program.cs
+++ CSharp-Advanced/{Final Exam} 11. Final Exam/AdvancedCSharpExam/02.Second/Program.cs
@@ -1,40 +1,67 @@
-﻿int size = int.Parse(Console.ReadLine());
+﻿int[] rowsNCols = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
 
-int[,] matrix = new int[size, size];
+string[,] playingField = new string[rowsNCols[0], rowsNCols[1]];
 
-/* Read a matrix from the console in the format:
- * 
- * 1 2 3 4
- * 4 3 2 1
- * 1 2 3 4
- * 4 3 2 1
- * 
- */
+int playerRow = 0;
+int playerCol = 0;
 
-void ReadMatrix(int[,] matrix, string separator)
+ReadMatrix(playingField);
+
+int movesTotal = 0;
+int targetsTouched = 0;
+
+string command = Console.ReadLine();
+
+while (command != "Finish" && targetsTouched < 3)
+{
+    if (command == "up" && playerRow - 1 >= 0 && playerRow - 1 < playingField.GetLength(0) && playingField[playerRow - 1, playerCol] != "O")
+    {
+        playerRow--;
+        movesTotal++;
+    }
+    else if (command == "down" && playerRow + 1 >= 0 && playerRow + 1 < playingField.GetLength(0) && playingField[playerRow + 1, playerCol] != "O")
+    {
+        playerRow++;
+        movesTotal++;
+    }
+    else if (command == "left" && playerCol - 1 >= 0 && playerCol - 1 < playingField.GetLength(1) && playingField[playerRow, playerCol - 1] != "O")
+    {
+        playerCol--;
+        movesTotal++;
+    }
+    else if (command == "right" && playerCol + 1 >= 0 && playerCol + 1 < playingField.GetLength(1) && playingField[playerRow, playerCol + 1] != "O")
+    {
+        playerCol++;
+        movesTotal++;
+    }
+
+    if (playingField[playerRow, playerCol] == "P")
+    {
+        targetsTouched++;
+        playingField[playerRow, playerCol] = "-";
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {targetsTouched} Moves made: {movesTotal}");
+
+void ReadMatrix(string[,] matrix)
 {
     for (int row = 0; row < matrix.GetLength(0); row++)
     {
-        int[] rowData = Console.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        string[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
 
         for (int col = 0; col < matrix.GetLength(1); col++)
         {
             matrix[row, col] = rowData[col];
+
+            if (matrix[row, col] == "B")
+            {
+                playerRow = row;
+                playerCol = col;
+            }
         }
-    }
-}
-
-//Print matrix
-
-void PrintMatrix<T>(T[,] matrix)
-{
-    for (int row = 0; row < matrix.GetLength(0); row++)
-    {
-        for (int col = 0; col < matrix.GetLength(1); col++)
-        {
-            Console.Write($"{matrix[row, col]} ");
-        }
-
-        Console.WriteLine();
     }
 }
---
+++ CSharp-Advanced/{Final Exam} 11. Final Exam/Problem Discriptions/SoftUniKindergarten_Skeleton_6.0/Kindergarten.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Registry = new List<Child>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Child> Registry { get; set; }
+
+        public int ChildrenCount { get {return Registry.Count; } }
+
+        public bool AddChild(Child child) 
+        {
+            if (Registry.Count < Capacity)
+            {
+                Registry.Add(child);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool RemoveChild(string childFullName)
+        {
+            foreach (var child in Registry)
+            {
+                string fullName = $"{child.FirstName} {child.LastName}";
+
+                if (fullName == childFullName)
+                {
+                    Registry.Remove(child);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Child GetChild(string childFullName)
+        {
+            foreach (var child in Registry)
+            {
+                string fullName = $"{child.FirstName} {child.LastName}";
+
+                if (fullName == childFullName)
+                {
+                    return child;
+                }
+            }
+
+            return null;
+        }
+
+        public string RegistryReport() 
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Registry = Registry.OrderByDescending(k => k.Age).ThenBy(k => k.LastName).ThenBy(k => k.FirstName).ToList();
+
+            sb.AppendLine($"Registered children in {this.Name}:");
+
+            foreach (var child in Registry)
+            {
+                sb.AppendLine(child.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
